7b118ff63ea9e176f659e6e9caa7adc8a49520ca
Evan Coleman <e@edc.me>
2022-06-02 20:14:46 -0400

    fix

--- Package.swift
+++ Package.swift
@@ -4,16 +4,23 @@ import PackageDescription
 
 let package = Package(
     name: "Utilities",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+    ],
     products: [
         .library(
             name: "Utilities",
             targets: ["Utilities"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.4"),
+    ],
     targets: [
         .target(
             name: "Utilities",
-            dependencies: []
+            dependencies: [.product(name: "Introspect", package: "SwiftUI-Introspect")]
         ),
         .testTarget(
             name: "UtilitiesTests",
